c788cebfb4750cac672b9b6527eec32ba452a6a9
DanielSD200 <68423270+DanielSD200@users.noreply.github.com>
2022-12-01 09:16:50 +0000

    Add files via upload
    Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+var calorieInput = File.ReadAllText(@"input.txt");
+string[] elvesCalories = calorieInput.Split("\r\n\r\n", System.StringSplitOptions.RemoveEmptyEntries);
+
+List<int> calorieTotals = new List<int>();
+
+for (int i = 0; i < elvesCalories.Count(); i++)
+{
+    var elfCalorieTotal = (Array.ConvertAll(elvesCalories[i].Split("\r\n"), s => Int32.Parse(s))).ToList().Sum();
+
+    calorieTotals.Add(elfCalorieTotal);
+}
+
+var highestCalories = calorieTotals.OrderDescending().FirstOrDefault();
+
+var topThreeCalories = calorieTotals.OrderDescending().Take(3).ToList().Sum();
+
+Console.WriteLine(highestCalories);
+
+Console.WriteLine(topThreeCalories);
