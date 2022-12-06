aa46dfddda9d98b578800d7e0af8133c03d2ed9f
Ellis,Daniel <daniel.ellis@uk.aswatson.com>
2022-12-06 11:33:36 +0000

    Day 6 Commit

---
+++ Day6/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+var input = File.ReadAllText(@"C:\Work\AdventOfCode22\AoC22\Day6\input.txt");
+
+int part1MarkerLength = 4;
+int part2MarkerLength = 14;
+
+for (int i = 0; i < input.Length -1; i++)
+{
+    string markerCheck = input.Substring(i, part2MarkerLength);
+
+    if (DuplicateCheck(markerCheck))
+    {
+        continue;
+    }
+    else
+    {
+        Console.WriteLine($"{markerCheck} at position {i + part2MarkerLength}");
+        break;
+    }
+}
+
+static bool DuplicateCheck(string input)
+{
+    var set = new HashSet<char>();
+    return input.Any(x => !set.Add(x));
+}
